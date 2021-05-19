e207c7b5c3fa8c25d09305d9aa9b2b3fb42779a5
terry <terry@bocco.com.tw>
2021-05-19 15:17:46 +0800

    Init blazor

---
+++ Country_Blazor/Data/CounterInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Country_Blazor.Data
+{
+    public class CounterInfo
+    {
+    }
+}
